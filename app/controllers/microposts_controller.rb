d5b736787b5b88bfa539e4b6582b342dc045729c
Michael Graham <lankage22@gmail.com>
2017-03-04 14:40:30 -0600

    Strip out image upload

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -22,7 +22,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content, :picture)
+      params.require(:micropost).permit(:content)
     end
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
